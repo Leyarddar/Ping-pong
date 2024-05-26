35d3e9dbc151672bf6a6894f41be457658aa89b5
Leyard <9611005445@mail.ru>
2024-05-26 15:30:49 +0300

    Update v2.0 ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -1,33 +1,23 @@
 from pygame import *
-from random import randint
-from time import time as timer
 
-back = (200, 225, 225)
-win_width = 700
+back = (255, 255, 255)
+win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 display.set_caption("Ping-Pong")
 window.fill(back)
 
 font.init()
-font1 = font.SysFont('Arial', 36)
-font2 = font.SysFont('Arial', 80)
-win = font2.render('MISSION COMPLETED', True, (255, 255, 255))
-lose = font1.render('MISSION FAILED', True, (180, 0, 0))
+font = font.SysFont('Arial', 36)
 
-mixer.init()
-mixer.music.load('space_war.mp3')
-mixer.music.play()
-
-bullets_music = mixer.Sound('fire.ogg')
+lose1 = font.render('FAILED player 1', True, (180, 0, 0))
+lose2 = font.render('FAILED player 2', True, (180, 0, 0))
+#mixer.init()
+#mixer.music.load('space_war.mp3')
+#mixer.music.play()
 
 clock = time.Clock()
-FPS = 144
-
-lost = 0
-score = 0
-max_lost = 3
-life = 3
+FPS = 60
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -41,17 +31,25 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update_l(self):
         keys = key.get_pressed()
-        if keys[K_a] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_d] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
-    def fire(self):
-        bullet = Bullet('bullet.png', self.rect.centerx, self.rect.top, 15, 20, 15)
-        bullets.add(bullet)
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
 
-player = Player('rocket.png', 5, 420, 80, 100, 5)
+speed_x = 3
+speed_y = 3
+
+player1 = Player('Racket.png', 30, 200, 20, 100, 10)
+player2 = Player('Racket.png', 520, 200, 20, 100, 10)
+ball = GameSprite('ball.png', 200, 200, 50, 50, 50)
 game = True
 finish = False
 while game:
@@ -59,15 +57,26 @@ while game:
         if e.type == QUIT:
             game = False
     if finish != True:
-        window.blit(background, (0, 0))
-        player.reset()
-        player.update()
-        if score >= 10:
+        window.fill(back)
+        player1.update_l()
+        player2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(player1, ball) or sprite.collide_rect(player2, ball):
+            speed_x *= -1
+            speed_y *= 1
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+        if ball.rect.x < 0:
             finish = True
-            window.blit(win, (200, 200))
-        text_lose = font1.render("Пропущено: " + str(lost), 1, (255, 255, 255))
-        text_score = font1.render("Счёт: " + str(score), 1, (255, 255, 255))
-        window.blit(text_lose, (10, 50))
-        window.blit(text_score, (10, 20))
+            window.blit(lose1, (200, 200))
+            game = True
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game = True
+        player1.reset()
+        player2.reset()
+        ball.reset()
     display.update()
     clock.tick(FPS)
